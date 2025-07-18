192f1b327e5a67848bc2f53c8c0a0fdb5dec7960
Abdallah-Mushtaha <abood.r.mushtaha@gmail.com>
2025-07-18 19:02:44 +0300

    build Favorite page and ther Feauter Modify Add to Cart or retreat

--- src/Containers/Favorites.jsx
+++ src/Containers/Favorites.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { CartContext } from "../Components/context/cartContext";
-import SlideProductes from "../Components/sideProductes/SlideProductes";
+import SlideProductes from "../Components/sideProductes/slideProductes";
 
 export default function Favorites() {
   //  will get the cart items from the context
